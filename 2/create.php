883539c585d325b9e7b605b410a9f95973125e49
basseleski <basseleski@gmail.com>
2017-09-06 15:17:01 +0200

    create stuff added

--- 2/create.php
+++ 2/create.php
@@ -4,6 +4,22 @@
 		<title>Speelvorm 2</title>
 	</head>
 	<body>
-		
+		<div>
+			<table>
+				<tr>
+					<td>
+						<label>Naam:</label>
+					</td>
+					<td>
+						<input type="text" name="">
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">
+						<input type="submit" value="Maak Lobby" name="makeLobbyButton">
+					</td>
+				</tr>
+			</table>
+		</div>
 	</body>
 </html>
