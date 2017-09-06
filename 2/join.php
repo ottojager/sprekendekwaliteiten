d9725a388e82302c99a24e580e7a9aba0a39e61b
basseleski <basseleski@gmail.com>
2017-09-06 14:58:29 +0200

    join stuff added

--- 2/join.php
+++ 2/join.php
@@ -4,5 +4,32 @@
 		<title>Speelvorm 2</title>
 	</head>
 	<body>
+		<div>
+			<form>
+				<table>
+					<tr>
+						<td>
+							<label>Naam:</label>
+						</td>
+						<td>
+							<input type="text" name="">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<label>code:</label>
+						</td>
+						<td>
+							<input type="text" name="">
+						</td>
+					</tr>
+					<tr>
+						<td colspan="2">
+							<input type="submit" value="join" name="joinButton">
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
 	</body>
 </html>
