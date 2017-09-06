08c400340b18d15360b9fe145d1bfa7a4fa739ed
basseleski <basseleski@gmail.com>
2017-09-06 13:49:40 +0200

    div met button om lobby te maken en joinen toegevoegt

--- 2/index.php
+++ 2/index.php
@@ -4,7 +4,9 @@
 		<title>Speelvorm 2</title>
 	</head>
 	<body>
-		
+		<div>
+			<button onclick="makeLobby">join een lobby</button>
+			<button onclick="joinLobby">maak een lobby</button>
+		</div>
 	</body>
 </html>
-
