2c158bd283cb7791e45930c9763c11755e3da92f
basseleski <basseleski@gmail.com>
2017-09-06 14:03:49 +0200

    buttons weer weg halen

--- 2/index.php
+++ 2/index.php
@@ -4,9 +4,5 @@
 		<title>Speelvorm 2</title>
 	</head>
 	<body>
-		<div>
-			<button onclick="makeLobby">join een lobby</button>
-			<button onclick="joinLobby">maak een lobby</button>
-		</div>
 	</body>
 </html>
