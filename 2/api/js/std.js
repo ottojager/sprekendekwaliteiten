4317abd9e643d1e3d6210a81d3f8adce1a3249cf
basseleski <basseleski@gmail.com>
2017-09-07 14:52:50 +0200

    changed url in GET to get correct file, made it target correct div and lowered refresh interval

--- 2/api/js/std.js
+++ 2/api/js/std.js
@@ -2,13 +2,13 @@ function start_update() {
 	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-     		document.getElementById("demo").innerHTML = this.responseText;
+     		document.getElementById("main").innerHTML = this.responseText;
 		}
   	};
-	xhttp.open("GET", "../check.php", true);
+	xhttp.open("GET", "http://localhost/kwal-spel/2/api/check.php", true);
 	xhttp.send();
 }
 
 window.setInterval(function(){
   start_update();
-}, 5000);
+}, 3000);
