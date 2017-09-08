e9c38964c04ae7ca860e50b9a3e55be38e8fdf1c
basseleski <basseleski@gmail.com>
2017-09-08 12:01:02 +0200

    added code to check if game is started and redirect to game page in lobby.php

--- 2/lobby.php
+++ 2/lobby.php
@@ -2,6 +2,14 @@
 	<head>
 		<title>Speelvorm 2</title>
 		<script src="api/js/std.js"></script>
+		<script>
+			window.setInterval(function(){
+				start_update();
+				if (game_info["game_started"] == true) {
+					window.location.href = 'game.php';
+				}
+			}, 3000);
+		</script>
 	</head>
 	<body>
 		<div id="main">
