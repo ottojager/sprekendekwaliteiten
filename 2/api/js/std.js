0db44cf0dcb6af3e4a874315b6624471c8201114
basseleski <basseleski@gmail.com>
2017-09-08 11:27:45 +0200

    made game json file accesable in the client

--- 2/api/js/std.js
+++ 2/api/js/std.js
@@ -2,7 +2,8 @@ function start_update() {
 	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-     		document.getElementById("main").innerHTML = this.responseText;
+     		//document.getElementById("main").innerHTML = this.responseText;
+		 	game_info = JSON.parse(this.responseText);
 		}
   	};
 	xhttp.open("GET", "http://localhost/kwal-spel/2/api/check.php", true);
@@ -12,5 +13,5 @@ function start_update() {
 start_update();
 
 window.setInterval(function(){
-  start_update();
+   start_update();
 }, 3000);
