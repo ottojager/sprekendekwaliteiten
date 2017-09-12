5fc505a55b1ad1e246d427ccd41594a648c58935
oddnightsky <nielsberichten@gmail.com>
2017-09-12 12:02:44 +0200

    forced $json to array in start.php

--- 2/api/start.php
+++ 2/api/start.php
@@ -3,7 +3,7 @@
 session_start();
 if ($_SESSION['player_id'] == 9 && isset($_SESSION['game_id'])) {
 	$game = $_SESSION['game_id'];
-	$json = json_decode(file_get_contents("../games/$game.json"));
+	$json = (array)json_decode(file_get_contents("../games/$game.json"));
 	$json['game_started'] = true;
 	file_put_contents("../games/$game.json", json_encode($json));
 } else {
