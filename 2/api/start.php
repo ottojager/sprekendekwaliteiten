86239683bda9a699d2b8cf2c2329fd41ef39dffc
oddnightsky <nielsberichten@gmail.com>
2017-09-12 11:50:18 +0200

    fixed some miscalled variables & added a 403 responce from start.php

--- 2/api/start.php
+++ 2/api/start.php
@@ -1,9 +1,12 @@
 <?php
 // file called to start the game
-if ($_SESSION['playerID'] == 9 && isset($_SESSION['game_id']))) {
-	$game = $_SESSION['game'];
+session_start();
+if ($_SESSION['player_id'] == 9 && isset($_SESSION['game_id'])) {
+	$game = $_SESSION['game_id'];
 	$json = json_decode(file_get_contents("../games/$game.json"));
 	$json['game_started'] = true;
 	file_put_contents("../games/$game.json", json_encode($json));
+} else {
+	header('HTTP/1.1 403 Forbidden');
 }
 ?>
