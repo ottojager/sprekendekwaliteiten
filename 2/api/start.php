066f13cd066e7ce6261e5bb0ff1570e1d782e502
basseleski <basseleski@gmail.com>
2017-09-13 14:15:36 +0200

    added importing and shuffling cardstack from database to create.php

--- 2/api/start.php
+++ 2/api/start.php
@@ -6,6 +6,16 @@ if ($_SESSION['player_id'] == 9 && isset($_SESSION['game_id'])) {
 	$game = $_SESSION['game_id'];
 	$json = (array)json_decode(file_get_contents("../games/$game.json"));
 	$json['game_started'] = true;
+	$db = mysqli_connect('localhost', 'root');
+	mysqli_select_db($db, "kwaliteitenspel");
+	$sql = "SELECT * FROM cards";
+	$result = mysqli_query($db, $sql);
+	$card_stack = array();
+	while ($card = mysqli_fetch_assoc($result)) {
+	    $card_stack[] = $card['name'];
+	}
+	shuffle($card_stack);
+	$json['card_stack'] = $card_stack;
 	file_put_contents("../games/$game.json", json_encode($json));
 } else {
 	header('HTTP/1.1 403 Forbidden');
