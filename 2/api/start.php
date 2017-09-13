b6db42708e945d6977f409ddab435b87b6e27cd6
oddnightsky <nielsberichten@gmail.com>
2017-09-13 08:51:58 +0200

    added a TODO in start.php

--- 2/api/start.php
+++ 2/api/start.php
@@ -2,6 +2,7 @@
 // file called to start the game
 session_start();
 if ($_SESSION['player_id'] == 9 && isset($_SESSION['game_id'])) {
+	//TODO: add check for minimum amount of players
 	$game = $_SESSION['game_id'];
 	$json = (array)json_decode(file_get_contents("../games/$game.json"));
 	$json['game_started'] = true;
