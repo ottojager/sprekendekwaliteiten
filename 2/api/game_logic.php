ca5abd645755cfed630d89ec9883977d6d33cb92
basseleski <basseleski@gmail.com>
2017-09-15 15:11:11 +0200

    added checking if selected player is itself in game_logic.php

--- 2/api/game_logic.php
+++ 2/api/game_logic.php
@@ -5,6 +5,8 @@ $game = $_SESSION['game_id'];
 $json = json_decode(file_get_contents("../games/$game.json"), true);
 if ($_SESSION['player_id'] == $json['current_player']) {
 	$json['current_player'] = $json['current_player'] + 1;
+	if ($_GET['sel'] != $_SESSION['player_id']) {
+	}
 }
 file_put_contents("../games/$game.json", json_encode($json));
 // fopen("test.txt", "w", $_SESSION['game_id');
