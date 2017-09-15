b1444768693832b07565230ee5e114ab1ad9118f
basseleski <basseleski@gmail.com>
2017-09-15 15:14:14 +0200

    changed turn order code and made it loop to first player after last players turn is over

--- 2/api/game_logic.php
+++ 2/api/game_logic.php
@@ -4,10 +4,14 @@ $game = $_SESSION['game_id'];
 
 $json = json_decode(file_get_contents("../games/$game.json"), true);
 if ($_SESSION['player_id'] == $json['current_player']) {
-	$json['current_player'] = $json['current_player'] + 1;
 	if ($_GET['sel'] != $_SESSION['player_id']) {
 		$json['players'][$_GET['sel']]['stack'][] = $json['current_card'];
 		$json['current_card'] = array_shift($json['card_stack']);
+		if ($json['current_player'] == count($json['players']) - 1) {
+			$json['current_player'] = 0;
+		} else {
+			$json['current_player'] = $json['current_player'] + 1;
+		}
 	}
 }
 file_put_contents("../games/$game.json", json_encode($json));
