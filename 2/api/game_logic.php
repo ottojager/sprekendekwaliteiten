2bf5db7a02adfd27c096fe6e677fe8bedf25ef16
basseleski <basseleski@gmail.com>
2017-09-15 15:12:30 +0200

    added giving card to an other player and drawing new card in game_logic.php

--- 2/api/game_logic.php
+++ 2/api/game_logic.php
@@ -6,6 +6,8 @@ $json = json_decode(file_get_contents("../games/$game.json"), true);
 if ($_SESSION['player_id'] == $json['current_player']) {
 	$json['current_player'] = $json['current_player'] + 1;
 	if ($_GET['sel'] != $_SESSION['player_id']) {
+		$json['players'][$_GET['sel']]['stack'][] = $json['current_card'];
+		$json['current_card'] = array_shift($json['card_stack']);
 	}
 }
 file_put_contents("../games/$game.json", json_encode($json));
