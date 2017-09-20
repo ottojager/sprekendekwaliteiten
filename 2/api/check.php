4ca57a7ed07a17287dcc523f9adec1cf41203bcd
oddnightsky <nielsberichten@gmail.com>
2017-09-20 11:29:39 +0200

    made it so people not in a game get a 403 when trying to access check.php

--- 2/api/check.php
+++ 2/api/check.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-$game = $_SESSION['game_id'];
-$json = file_get_contents("../games/$game.json");
-echo $json;
+if (isset($_SESSION['game_id'])) {
+	$game = $_SESSION['game_id'];
+	$json = file_get_contents("../games/$game.json");
+	echo $json;
+} else {
+	header('HTTP/1.1 403 Forbidden');
+}
 ?>
