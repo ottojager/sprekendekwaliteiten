2f93a7db6f171746a6a3c47d580b3b15d4cc7fe6
basseleski <basseleski@gmail.com>
2017-09-22 11:15:07 +0200

    added lotso changes(blame niels if this breaks stuff) in game.php

--- 2/game.php
+++ 2/game.php
@@ -1,10 +1,18 @@
-<?php session_start(); ?>
+<?php
+session_start();
+if (!isset($_SESSION['game_id'])) {
+	header('Location: ./');
+}
+$game = $_SESSION['game_id'];
+$json = json_decode(file_get_contents("./games/$game.json"), true);
+?>
 <!DOCTYPE html>
 <html lang="nl=NL">
 	<head>
 		<title>Speelvorm 2</title>
 		<script src="api/js/std.js"></script>
         <script>
+			var amount_players = <?php echo count($json['players']);?>;
 			window.setInterval(function(){
 				start_update();
 				document.getElementById("current_card").innerHTML = game_info['current_card'];
@@ -16,7 +24,37 @@
 					child.innerHTML = item;
 					list.appendChild(child);
 				});
-			}, 2000);
+
+				//changing player list order
+				//check if the displayer player list doesn't match the new one
+				if (game_info['current_player'] != document.getElementById("player_list").firstElementChild.id) {
+					//fill player list inits
+					var player_list = document.getElementById('player_list');
+					player_list.innerHTML = '';
+					console.log('cleared player list');
+					//fill list starting with the current till last player
+					for (var i = game_info['current_player']; i < amount_players; i += 1) {
+						console.log('adding player to list:' + i);
+						var li = document.createElement('li');
+						li.innerHTML = game_info['players'][i]['name'];
+						li.id = i;
+						player_list.appendChild(li);
+					};
+					//check if player = 0 becuase this code is then useless
+					if (game_info['current_player'] > 0) {
+						//fill list starting with first player to the player before the current one
+						for (var i = 0; i < game_info['current_player']; i++) {
+							console.log('adding player to list 2:' + i);
+							var li = document.createElement('li');
+							li.innerHTML = game_info['players'][i]['name'];
+							li.id = i;
+							player_list.appendChild(li);
+						};
+					};
+
+					addListeners(amount_players);
+				};
+			}, 5000);
 		</script>
         <link rel="stylesheet" href="css/game.css" type="text/css">
         <link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
@@ -26,8 +64,6 @@
 		<p id="current_card"></p>
         <ul id="player_list">
             <?php
-            $game = $_SESSION['game_id'];
-            $json = json_decode(file_get_contents("./games/$game.json"), true);
             foreach ($json['players'] as $key => $value) {
                 echo '<li id="'.$value['player_id'].'">'.$value['name'].'</li>';
             }
@@ -37,4 +73,6 @@
         </ul>
 	</body>
 </html>
-<script>addListeners(<?php echo count($json['players']);?>)</script>
+<script>addListeners(amount_players);
+//alert(document.getElementById("player_list").firstElementChild.text);
+</script>
