fbd681e7290be80236dab0402b7d41a2e1a97aa4
Niels van der Mark <nielsberichten@gmail.com>
2017-10-03 14:10:53 +0200

    this commit shouldn't truely change anything but edits everyfile I recursivly changed file permisions on my system

---
+++ 2/end/pdf.php
@@ -0,0 +1,11 @@
+<?php
+require('../../fpdf/fpdf.php');
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial', 'B', 16); // set various font options (font family, style, size)
+$pdf->Cell(0, 10, 'Kwaliteiten spell', 0, 1, 'C'); // add the text in a 10 heigh cell without a border that's centered
+
+$pdf->Output(); // sends the PDF to the user
+                // browsers with a build in PDF viewer will show it
+				// other browsers will download it
+ ?>
