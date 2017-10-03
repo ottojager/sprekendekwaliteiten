41ee46b62738decbd5668243a3f8fc1c086f9fdc
Niels van der Mark <nielsberichten@gmail.com>
2017-10-03 15:23:05 +0200

    added more pdf stuff and def don't want to kill myself

--- 2/end/pdf.php
+++ 2/end/pdf.php
@@ -1,9 +1,20 @@
 <?php
+session_start();
 require('../../fpdf/fpdf.php');
+
+$game = $_SESSION['game_id'];
+$player = $_SESSION['player_id'];
+$json = json_decode(file_get_contents("../games/$game.json"), true);
+
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16); // set various font options (font family, style, size)
 $pdf->Cell(0, 10, 'Kwaliteiten spell', 0, 1, 'C'); // add the text in a 10 heigh cell without a border that's centered
+
+$pdf->SetFont('Arial', '', 12); // reset font to something more standard
+foreach ($json['players'][$player]['stack'] as $key => $value) {
+	$pdf->Cell(0, 6, $value, 0, 1, 'C');
+}
 
 $pdf->Output(); // sends the PDF to the user
                 // browsers with a build in PDF viewer will show it
