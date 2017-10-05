10408cca21a56bf6c26153ae90fdb1f1847878c0
Stefan v V <svveldhuizen@edu.bartimeus.nl>
2017-10-05 15:13:10 +0200

    Today was a good day, more banned words

--- 2/create.php
+++ 2/create.php
@@ -5,7 +5,8 @@ if (isset($_POST['makeLobbyButton'])) {
 		//list of filtered codes
 		$filtered_names = array (
 			'LUL','KUT','PIK','SEX','FUC','FUK','SUC','KKK','GAY','FAG','NIG','ZAK','POO','PIS','DIK','KOK','COK','ASS',
-			'TIT','JIZ','CUM','BSD','WIN','MAC','IOS','GOY','STD','NAZ','NZI','HEL','GUN','BOM','PRN'
+			'TIT','JIZ','CUM','BSD','WIN','MAC','IOS','GOY','STD','NAZ','NZI','HEL','GUN','BOM','PRN','WWI','JAP','NIP',
+			'NAP','WAR','WII',
 		);
 		// generate game id
 		do {
@@ -51,6 +52,7 @@ if (isset($_POST['makeLobbyButton'])) {
 				<div id="formstyle">
 					<label>Naam:</label>
 					<input type="text" name="name">
+					<input type="number" name="cards">
 					<input type="submit" value="Maak Lobby" name="makeLobbyButton">
 				</div>
 			</form>
