334e99b1b205abd4b6921c5a2ac670cb2e5e35b5
basseleski <basseleski@gmail.com>
2017-10-06 13:06:42 +0200

    changed turn-action to turn_action in game_logic.php

--- 2/api/game_logic.php
+++ 2/api/game_logic.php
@@ -8,7 +8,7 @@ if ($_SESSION['player_id'] == $json['current_player']) {
 	//check if player didn't select themself
 	if ($_GET['sel'] != $_SESSION['player_id']) {
 		//add to game info which player is selected
-		array_push($json['turn-action'], $_GET['sel']);
+		array_push($json['turn_action'], $_GET['sel']);
 		//add card to chosen players cards and draw new card
 		$json['players'][$_GET['sel']]['stack'][] = $json['current_card'];
 		$json['current_card'] = array_shift($json['card_stack']);
