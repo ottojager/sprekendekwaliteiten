e7e2fb23262f227fbac818c2d1eea1c3abca1d7d
Niels van der Mark <nielsberichten@gmail.com>
2017-10-10 13:41:36 +0200

    fixed grammer in a comment

--- 2/end/pdf.php
+++ 2/end/pdf.php
@@ -17,7 +17,7 @@ if (!(bool)$json) { // if the json is empty or not existant
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16); // set various font options (font family, style, size)
-$pdf->Cell(0, 10, 'Kwaliteiten spell', 0, 1, 'C'); // add the text in a 10 heigh cell without a border that's centered
+$pdf->Cell(0, 10, 'Kwaliteiten spell', 0, 1, 'C'); // add text in a new 10 heigh centered cell without a border
 
 $pdf->SetFont('Arial', '', 12); // reset font to something more standard
 foreach ($json['players'][$player]['stack'] as $key => $value) {
