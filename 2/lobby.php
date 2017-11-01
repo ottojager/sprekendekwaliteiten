6e687b6330832f3d67a8819638221ae8ebf01ef7
Niels van der Mark <nielsberichten@gmail.com>
2017-11-01 14:00:24 +0100

    did all the stuff to kick players

--- 2/lobby.php
+++ 2/lobby.php
@@ -36,26 +36,47 @@ if (!(bool)$json) { // if $json actually has content
 		<?php } // end of leader only javascript ?>
 
 		<script>
+			var id = <?php echo $_SESSION['player_id']; ?>;
+
 			window.setInterval(function(){
 				start_update();
 				if (game_info["game_started"] == true) {
 					window.location.href = 'game.php';
 				}
+
+				if (game_info['players'][id] == []) { // when the user doesn't "exist"
+					window.location.href = './';
+				}
+
 				//document.getElementById("leader").innerHTML = game_info["leader_name"];
 				var list = document.getElementById("player_list");
 				list.innerHTML = '';
 				game_info["players"].forEach(function(item, index){
 					var child = document.createElement('li');
 					child.innerHTML = item['name'];
+
+					<?php if ($_SESSION['player_id'] == 11) { ?>
+						var button = document.createElement('button');
+						button.onclick = function() {
+							var xhttp = new XMLHttpRequest();
+							xhttp.open("GET", "http://localhost/kwal-spel/2/api/kick.php?p="+index, false);
+							xhttp.send();
+						}
+						button.innerHTML = 'X';
+						child.appendChild(button);
+					<?php } ?>
+
 					list.appendChild(child);
 				});
-				document.getElementById("game_id").innerHTML = game_info['game_id'];
-				document.getElementById("leader").innerHTML = 'Leider: ' + game_info['leader_name'];
+
+				// these never update and are thus not needed
+				//document.getElementById("game_id").innerHTML = game_info['game_id'];
+				//document.getElementById("leader").innerHTML = 'Leider: ' + game_info['leader_name'];
 				//document.getElementById("player_list").innerHTML = game_info[""];
 			}, 3000);
 		</script>
-        <link rel="stylesheet" href="css/lobby_stylesheet.css" type="text/css">
-        <link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
+	<link rel="stylesheet" href="css/lobby_stylesheet.css" type="text/css">
+	<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
 	</head>
 	<body>
 		<div id="main">
@@ -64,7 +85,11 @@ if (!(bool)$json) { // if $json actually has content
 			echo '<p id="leader">Leider: '.$json['leader_name'].'</p>';
 			echo '<ol id="player_list">';
 			foreach ($json['players'] as $key => $value) {
-				echo '<li>'.$value['name'].'</li>';
+				if ($_SESSION['player_id'] == 11) {
+					echo '<li>'.$value['name'].'<button onclick="alert(\''.$key.'\')">X</button></li>';
+				} else {
+					echo '<li>'.$value['name'].'</li>';
+				}
 			}
 			echo '</ol>';
 			?>
