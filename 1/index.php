a4a132f56419e8433be35978db53dd163cea7f4d
Stefan v V <svveldhuizen@edu.bartimeus.nl>
2017-11-17 11:46:14 +0100

    Redesign css/renaming imgs

--- 1/index.php
+++ 1/index.php
@@ -28,25 +28,25 @@
 		<h1>Kwaliteitenspel - Bewustwording</h1>
 		<div id="top" class="row">
 			<h2 class="col-xs-1 col-sm-2 col-sm-offset-2" style="text-align: right;">Actieve kaart:</h2>
-			<div id="huidig" class="col-xs-11 col-sm-6 col-md-4"><img src="kaart-liggend%20goed.png"><div id="current"></div></div>
+			<div id="huidig" class="col-xs-11 col-sm-6 col-md-4"><img src="kaart-liggend_goed.png"><div id="current"></div></div>
 		</div>
 		<div>
 		<h2 id="jkaart">Jouw 8 kaarten</h2>
 		<ul>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot1"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot2"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot3"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot4"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot5"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot6"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot7"></button></li>
-			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend%20goed.png"><button id="slot8"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot1"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot2"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot3"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot4"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot5"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot6"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot7"></button></li>
+			<li class="col-xs-12 col-sm-6 col-md-3"><img src="kaart-liggend_goed.png"><button id="slot8"></button></li>
 		</ul>
 		</div>
 		<div style="clear: both;"></div>
-		<div id="onmaken" class="col-xs-12 col-sm-6 col-md-3"><img src="knop%20goed.png" alt="Ongedaan maken"><button id="backButton" onclick="backButton(this)">Ongedaan maken</button></div>
-		<a class="skiplink col-xs-12 col-sm-6 col-md-3" href="#current">Naar actieve kaart</a>
-		<div id="eindspel" class="col-xs-12 col-sm-6 col-md-3"><img src="knop%20goed.png" alt="Beëindig spel"><button id="endGame" onclick="endGame()">Be&#235;indig spel</button></div>
+		<div id="onmaken" class="col-xs-12 col-sm-6 col-md-3"><img src="knop_goed.png" alt="Ongedaan maken"><button id="backButton" onclick="backButton(this)">Ongedaan maken</button></div>
+		<div class="col-xs-12 col-sm-6 col-md-3"><a class="skiplink" href="#current">Naar actieve kaart</a></div>
+		<div id="eindspel" class="col-xs-12 col-sm-6 col-md-3"><img src="knop_goed.png" alt="Beëindig spel"><button id="endGame" onclick="endGame()">Be&#235;indig spel</button></div>
 		<ul class="col-xs-12 col-sm-6 col-md-3" id="graveyard" style="clear:both;">
 			<li><h2>Afleg stapel</h2></li>
 		</ul>
