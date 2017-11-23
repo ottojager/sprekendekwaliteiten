1360f8903eb2370ec8f717f3eb20c19a6d2573c7
Niels van der Mark <nielsberichten@gmail.com>
2017-11-23 10:37:41 +0100

    added the index.php also removed the now uneeded main.css

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl=NL">
+	<head>
+		<title>Kwaliteitspel</title>
+	</head>
+	<body>
+		<h1>Kwaliteitspel</h1>
+		<h2>welkom bij het kwaliteitenspel</h2>
+		<p></p>
+		<a href="./bewustwording">Speel alleen</a>
+		<a href="./feedback/join.php">Speel samen</a>
+		<a href="./feedback/create.php">Begeleider? Klik hier</a>
+	</body>
+</html>
