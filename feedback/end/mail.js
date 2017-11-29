218c945faa9cbd0c5c09ae9348298b84bb41a199
Niels van der Mark <nielsberichten@gmail.com>
2017-11-29 12:20:37 +0100

    improved error messages in mail.js also added a TODO that needs to be looked at

--- feedback/end/mail.js
+++ feedback/end/mail.js
@@ -1,12 +1,13 @@
 function send_mail() {
-	var email = document.getElementById('email').value;
+	var email = document.getElementById('email').value; //TODO: check for empty email
+														// Het verplichte veld \'Email\' is niet ingevuld.
 	var pattern =
 /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // checks for valid email adderess
 	var match = pattern.test(email);
 	var p = document.getElementById('error');
 	if (!match) {
-		alert('Het ingevulde email adderess klopt niet');
+		alert('Het veld \'Email\' is niet goed ingevuld.\nZorg dat het een correct formaat heeft bijvoorbeeld \'naam@domein.nl\'.');
 		exit();
 	} else {
 		console.log('check passed')
@@ -25,6 +26,5 @@ function send_mail() {
 		xhttp.open('POST', './mail.php', true);
 		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xhttp.send('email='+email); // put any post values in here
-									// make sure they're propperly encoded
 	}
 }
