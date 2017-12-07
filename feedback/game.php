41b50f9785dbb23bbe210824f19a15579019246e
Stefan v V <svveldhuizen@edu.bartimeus.nl>
2017-12-07 10:55:17 +0100

    O

--- feedback/game.php
+++ feedback/game.php
@@ -196,7 +196,7 @@ if ($_SESSION['player_id'] != 11) {
 				<?php echo $json['players'][$json['current_player']]['name']." is aan de beurt." ?>
 			</div>
 
-			<div id="card_active" class="col-xs-1 col-sm-1 col-md-2 col-md-offset-1">Actieve kaart:</div>
+			<div id="card_active" class="col-xs-1 col-sm-1 col-md-2">Actieve kaart:</div>
 			<div id="card_display" class="col-xs-11 col-sm-6 col-md-5"><p id="current_card"></p></div>
 
 			<ul id="player_list" class="col-xs-11 col-sm-6 col-md-5">
