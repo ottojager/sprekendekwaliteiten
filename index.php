e8de43243689578b640eaa0e3b07e22200879a2e
ThotEradicator <fkock@edu.bartimeus.nl>
2018-09-06 15:19:57 +0200

    semi-final header release V0.1
    *SPOILERS* kleine aanpassingen nodig bij de onderste H1

--- index.php
+++ index.php
@@ -9,6 +9,29 @@
 		<title>Kwaliteitenspel</title>
 	</head>
 	<body>
+		<header>
+			<h1 class="titel">SPREKENDE</h1>
+			<h1 class="titel onder">KWALITEITEN</h1>
+			<div class="headvorm">
+				<div class="headvorm-lesser"
+					 style="width:25%;">
+					<a style="display:block;">spel: combi</a>
+				</div>
+				<div class="headvorm-lesser"
+					 style="width:25%;">
+					<a style="display:block;">terug naar start</a>
+				</div>
+				<div class="headvorm-lesser" style="width:25%;"></div>
+				<div class="headvorm-lesser"
+					 style="width:25%;">
+					<a style="display:block;">help</a>
+				</div>
+				<div class="headvorm-lesser"
+					 style="width:10%;">
+					<a style="display:block;">naam speler</a>
+				</div>
+			</div>
+		</header>
 		<main class="container">
 			<div id="topbar"></div>
 				<div id="sidetopbar">
