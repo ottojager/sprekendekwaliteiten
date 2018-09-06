f29c7f5956a7298a1c2d0b5c6480bc90eb9d4a66
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-06 11:04:04 +0200

    619st: Added my super awesome footer α-version.

--- index.php
+++ index.php
@@ -7,18 +7,23 @@
 		<title>Kwaliteitenspel</title>
 	</head>
 	<body>
-		<div id="topbar"></div>
-			<div id="sidetopbar">
-				<div id="borderimage"></div>
-				<div id="player__name"></div>
+		<main class="container">
+			<div id="topbar"></div>
+				<div id="sidetopbar">
+					<div id="borderimage"></div>
+					<div id="player__name"></div>
+				</div>
+			<h1>Kwaliteitenspel</h1>
+			<h2>Welkom bij het Kwaliteitenspel</h2>
+			<p></p>
+			<div id="knoppen">
+				<a href="./bewustwording">Speel alleen</a>
+				<a href="./feedback/join.php">Speel samen</a>
+				<a href="./feedback/create.php">Begeleider? Klik hier</a>
 			</div>
-		<h1>Kwaliteitenspel</h1>
-		<h2>Welkom bij het Kwaliteitenspel</h2>
-		<p></p>
-		<div id="knoppen">
-			<a href="./bewustwording">Speel alleen</a>
-			<a href="./feedback/join.php">Speel samen</a>
-			<a href="./feedback/create.php">Begeleider? Klik hier</a>
-		</div>
+		</main>
 	</body>
+	<footer>
+		<p>"Sprekende kwaliteiten is uitgevoerd met toestemming van Peter Gerrickens en mogelijk gemaakt door Stichting Bartim&eacute;us Sonneheerdt en het KF Heinfonds"</p>
+	</footer>
 </html>
