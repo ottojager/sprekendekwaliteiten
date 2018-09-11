f90b7663424533a9885227be2d7b798e02f219bd
headphoneGear <fkock@edu.bartimeus.nl>
2018-09-11 10:26:48 +0200

    added new folder + header positioning test
    maybe a new Meistertask-task could be added for putting all images in the newly added folder. but thats to be decided later on if this is actually more convenient in the end than the other way the images are handled now.
    
    Header now has the inline-style removed for testing.

--- header.php
+++ header.php
@@ -2,21 +2,21 @@
 	<h1 class="titel boven">sprekende</h1>
 	<h1 class="titel onder">kwaliteiten</h1>
 	<div class="headvorm">
-		<div class="headvorm-lesser"
-			 style="width:25%;">
+		<div class="headvorm-lesser">
+			 <!--style="width:25%;"-->
 			<a style="display:block;">spel: combi</a>
 		</div>
-		<div class="headvorm-lesser"
-			 style="width:25%;">
+		<div class="headvorm-lesser">
+			 <!--style="width:25%;"-->
 			<a style="display:block;">terug naar start</a>
 		</div>
-		<div class="headvorm-lesser" style="width:25%;"></div>
-		<div class="headvorm-lesser"
-			 style="width:25%;">
+		<div class="headvorm-lesser"> <!--style="width:25%;"--></div>
+		<div class="headvorm-lesser">
+			 <!--style="width:25%;"-->
 			<a style="display:block;">help</a>
 		</div>
-		<div class="headvorm-lesser"
-			 style="width:10%;">
+		<div class="headvorm-lesser">
+			 <!--style="width:10%;" -->
 			<a style="display:block;">naam speler</a>
 		</div>
 	</div>
