cdc952821db4239a774d2a6f8139716dee0d228c
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-11 12:02:25 +0200

    Life sucks but so does this code.

--- bewustwording/index.php
+++ bewustwording/index.php
@@ -2,6 +2,7 @@
 <html lang="nl=NL">
 	<head>
 		<title>Speelvorm 1</title>
+		<link rel="stylesheet" href="../css/spelvorm1.css" type="text/css">
 		<link rel="stylesheet" href="../css/index.css" type="text/css">
 		<link rel="stylesheet" href="../css/header.css" type="text/css">
 		<link rel="stylesheet" href="../css/footer.css" type="text/css">
@@ -10,9 +11,9 @@
 	<body>
 		<?php include('../header.php') ?>
 		<main class="container">
-			<h1>Bewustwording spel</h1>
-			<p>Lorem Ipsum</p>
-			<a href="./game.php">start spel</a>
+			<h1 class="menu-sv1">Bewustwording spel</h1>
+			<p class="menu-sv1">Lorem Ipsum</p>
+			<a class="menu-sv1" href="./game.php">start spel</a>
 		</main>
 		<?php include('../footer.php') ?>
 	</body>
