d44efbef42f6b6762d9b9c2f633713344ef5e704
nvmark <nielsberichten@gmail.com>
2018-09-12 08:52:31 +0200

    initial very basic file with some stuff that has to exist to start on 3

---
+++ 3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Spelvorm 3</title>
+		<link rel="stylesheet" href="../css/index.css" type="text/css">
+		<link rel="stylesheet" href="../css/header.css" type="text/css">
+		<link rel="stylesheet" href="../css/footer.css" type="text/css">
+		<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
+	</head>
+	<body>
+		<?php include('../header.php') ?>
+		<main class="container">
+			<h1>Placeholder</h1>
+			<p>Holder of places</p>
+		</main>
+		<?php include('../footer.php') ?>
+	</body>
+</html>
