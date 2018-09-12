36164f99ec398665a65d730182d1f70c6c1c3fde
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-12 09:28:24 +0200

    Pre-Purchase bonus: Fixed basis.css en is nu in gebruik!

--- bewustwording/index.php
+++ bewustwording/index.php
@@ -2,8 +2,8 @@
 <html lang="nl=NL">
 	<head>
 		<title>Speelvorm 1</title>
+		<link rel="stylesheet" href="../css/basis.css" type="text/css">
 		<link rel="stylesheet" href="../css/spelvorm1.css" type="text/css">
-		<link rel="stylesheet" href="../css/index.css" type="text/css">
 		<link rel="stylesheet" href="../css/header.css" type="text/css">
 		<link rel="stylesheet" href="../css/footer.css" type="text/css">
 		<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
@@ -13,7 +13,7 @@
 		<main class="container">
 			<h1 class="menu-sv1">Bewustwording spel</h1>
 			<p class="menu-sv1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ligula vitae egestas luctus, dolor nulla aliquet elit, a aliquam magna nulla at odio. Aenean volutpat lorem sed molestie iaculis. Integer volutpat sapien nulla. Duis volutpat egestas quam, vel blandit quam sodales ut. Nulla tristique quam eget enim blandit, at viverra urna luctus. Proin sagittis, magna vehicula ullamcorper elementum, erat metus scelerisque lectus, id vestibulum velit sem condimentum neque. Mauris venenatis imperdiet mi. Duis quis justo non nisl ultricies blandit a nec mauris. Integer molestie, libero ac pellentesque aliquet, odio velit hendrerit</p>
-			<a class="menu-sv1" href="./game.php">start spel</a>
+			<a class="button" href="./game.php">start spel</a>
 		</main>
 		<?php include('../footer.php') ?>
 	</body>
