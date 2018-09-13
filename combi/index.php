9b8b42ccd36223cc39d9041cf0a722a9b876d65e
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-13 10:46:49 +0200

    Alpha versies van spelvorm 2 en 3's menu toegevoegd

--- combi/index.php
+++ combi/index.php
@@ -2,7 +2,8 @@
 <html>
 	<head>
 		<title>Spelvorm 3</title>
-		<link rel="stylesheet" href="../css/index.css" type="text/css">
+		<link rel="stylesheet" href="../css/basis.css" type="text/css">
+		<link rel="stylesheet" href="../css/spelvorm3.css" type="text/css">
 		<link rel="stylesheet" href="../css/header.css" type="text/css">
 		<link rel="stylesheet" href="../css/footer.css" type="text/css">
 		<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
@@ -10,9 +11,14 @@
 	<body>
 		<?php include('../header.php') ?>
 		<main class="container">
-			<h1>Placeholder</h1>
-			<p>Holder of places</p>
-		</main>
+			<div class="startscherm">
+				<h1 class="startscherm-content">Combi spel</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ligula vitae egestas luctus, dolor nulla aliquet elit, a aliquam magna nulla at odio. Aenean volutpat lorem sed molestie iaculis. Integer volutpat sapien nulla. Duis volutpat egestas quam, vel blandit quam sodales ut. Nulla tristique quam eget enim blandit, at viverra urna luctus. Proin sagittis, magna vehicula ullamcorper elementum, erat metus scelerisque lectus, id vestibulum velit sem condimentum neque. Mauris venenatis imperdiet mi.</p>
+			</div>
+			<div class="button-positie">
+				<a class="button" href="join.php">Speler</a>
+				<a class="button" href="create.php">Spelleider</a>
+			</div>
 		<?php include('../footer.php') ?>
 	</body>
 </html>
--- feedback/index.php
+++ feedback/index.php
@@ -6,16 +6,22 @@ session_start();
 	<head>
 		<title>Speelvorm 2</title>
 		<link rel="stylesheet" href="../css/basis.css" type="text/css">
+		<link rel="stylesheet" href="../css/spelvorm2.css" type="text/css">
 		<link rel="stylesheet" href="../css/header.css" type="text/css">
 		<link rel="stylesheet" href="../css/footer.css" type="text/css">
 		<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
 	</head>
 	<body>
 		<?php include('../header.php'); ?>
-		<h1>Feedback spel</h1>
-		<p>Lorum Ipsum</p>
-		<a href="join.php">Speler</a>
-		<a href="create.php">Spelleider</a>
+		<main class="container">
+			<div class="startscherm">
+				<h1 class="startscherm-content">Feedback spel</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ligula vitae egestas luctus, dolor nulla aliquet elit, a aliquam magna nulla at odio. Aenean volutpat lorem sed molestie iaculis. Integer volutpat sapien nulla. Duis volutpat egestas quam, vel blandit quam sodales ut. Nulla tristique quam eget enim blandit, at viverra urna luctus. Proin sagittis, magna vehicula ullamcorper elementum, erat metus scelerisque lectus, id vestibulum velit sem condimentum neque. Mauris venenatis imperdiet mi.</p>
+			</div>
+			<div class="button-positie">
+				<a class="button" href="join.php">Speler</a>
+				<a class="button" href="create.php">Spelleider</a>
+			</div>
 <?php	if (isset($_SESSION['game_id'])) {
 			$game = $_SESSION['game_id'];
 			$json = json_decode(file_get_contents("./games/$game.json"), true);
