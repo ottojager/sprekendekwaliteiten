d800b5bbe491677ef9c9d030b20718d4921c9ea6
nvmark <nielsberichten@gmail.com>
2018-09-14 10:44:23 +0200

    Aniversary commit

--- sql/table.sql
+++ sql/table.sql
@@ -1,3 +1,5 @@
+-- this code has remained un-edited for a year
+-- this is just here so I can commit a special commit for this occasion
 CREATE TABLE cards (
 	ID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	name varchar(30) NOT NULL
