8549175d8eb8c9b2b689cf0edd40cc6ac511096a
headphoneGear <fkock@edu.bartimeus.nl>
2018-09-20 15:24:35 +0200

    header media query not finished
    er moet nog jQurey/javascript toegevoegd worden. dat zal wel lukken, maar alleen even kijken of het kan en het geen conflict tussen andere, al bestaande code.

--- header.php
+++ header.php
@@ -8,23 +8,22 @@
 			<h1 class="titel onder">kwaliteiten</h1>
 		</div>
 	</div>
-	<div class="headvorm">
-		<div class="headvorm-lesser">
-			 <!--style="width:25%;"-->
-			<a style="display:block;">Spel: Combi</a>
-		</div>
-		<div class="headvorm-lesser">
-			 <!--style="width:25%;"-->
-			<a href="./index.php" style="display:block;">Terug naar start</a>
-		</div>
-		<!--<div class="headvorm-lesser" style="width:5%;"></div -->
-		<div class="headvorm-lesser">
-			 <!--style="width:25%;"-->
-			<a style="display:block;">Help</a>
-		</div>
-		<div class="headvorm-lesser">
-			 <!--style="width:10%;" -->
-			<a style="display:block;">Naam speler</a>
+	<div id="header-menu"> <!-- dit is de "container"-->
+		<button class="menu-toggle" id="menu-toggle"> <!-- klapt het menu in en uit, maar is alleen zichtbaar in de media query -->
+			<img src="http://reacursist.nl/dbconnect/wp-content/themes/dbconnect/../../uploads/2018/02/menu.png"> <!-- de hamburger -->
+		</button>
+		<div class="headvorm"> <!-- "menu-hoofdmenu-container", als je op de Doofblind manier kijkt. -->
+			<a class="headvorm-lesser"
+				 style="display:block;">Spel: Combi</a>
+			
+			<a class="headvorm-lesser" style="display:block;" href="./index.php">Terug naar start</a>
+			
+			<!--<div class="headvorm-lesser" style="width:5%;"></div -->
+			<a class="headvorm-lesser"
+				 style="display:block;">Help</a>
+				 <!--style="width:10%;" -->
+			<a style="display:block;" class="headvorm-lesser">Naam speler</a>
+			
 		</div>
 	</div>
 </header>
