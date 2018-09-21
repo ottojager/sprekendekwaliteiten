e53c3748e42a451a65d5cc712c3d3cb14562b628
headphoneGear <fkock@edu.bartimeus.nl>
2018-09-21 13:39:43 +0200

    comments verwijderd
    en de code ietsje overzichtelijker gemaakt

--- header.php
+++ header.php
@@ -1,29 +1,38 @@
 <header>
 	<div class="header-content">
 		<div class="header-content-parts">
-			<img class="logo" src="/sprekendekwaliteiten/afbeeldingen/logo-grote-schermen.png" alt="logo">
+			<img class="logo"
+				 src="/sprekendekwaliteiten/afbeeldingen/logo-grote-schermen.png"
+				 alt="logo">
 		</div>
 		<div class="header-content-parts">
 			<h1 class="titel boven">sprekende</h1>
 			<h1 class="titel onder">kwaliteiten</h1>
 		</div>
 	</div>
-	<div id="header-menu"> <!-- dit is de "container"-->
-		<button class="menu-toggle" id="menu-toggle"> <!-- klapt het menu in en uit, maar is alleen zichtbaar in de media query -->
-			<img src="http://reacursist.nl/dbconnect/wp-content/themes/dbconnect/../../uploads/2018/02/menu.png"> <!-- de hamburger -->
+	<div id="header-menu">
+		<button class="menu-toggle"
+				id="menu-toggle">
+			<img src="http://reacursist.nl/dbconnect/wp-content/themes/dbconnect/../../uploads/2018/02/menu.png"> 
 		</button>
-		<div class="headvorm"> <!-- "menu-hoofdmenu-container", als je op de Doofblind manier kijkt. -->
+		<div class="headvorm"> 
 			<a class="headvorm-lesser"
-				 style="display:block;">Spel: Combi</a>
-			
-			<a class="headvorm-lesser" style="display:block;" href="./index.php">Terug naar start</a>
-			
-			<!--<div class="headvorm-lesser" style="width:5%;"></div -->
+			   style="display:block;">
+				Spel: Combi
+			</a>
 			<a class="headvorm-lesser"
-				 style="display:block;">Help</a>
-				 <!--style="width:10%;" -->
-			<a style="display:block;" class="headvorm-lesser">Naam speler</a>
-			
+			   style="display:block;"
+			   href="./index.php">
+				Terug naar start
+			</a>
+			<a class="headvorm-lesser"
+			   style="display:block;">
+				Help
+			</a>
+			<a style="display:block;"
+			   class="headvorm-lesser">
+				Naam speler
+			</a>
 		</div>
 	</div>
 </header>
