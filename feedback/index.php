c145f535138e2b915b1b94b4b75830865d8abb0d
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-21 12:04:43 +0200

    Weet je, dit werkt. Laat mij met rust.

--- feedback/index.php
+++ feedback/index.php
@@ -17,23 +17,24 @@ session_start();
 			<div class="startscherm">
 				<h1 class="startscherm-content">Feedback spel</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus, ligula vitae egestas luctus, dolor nulla aliquet elit, a aliquam magna nulla at odio. Aenean volutpat lorem sed molestie iaculis. Integer volutpat sapien nulla. Duis volutpat egestas quam, vel blandit quam sodales ut. Nulla tristique quam eget enim blandit, at viverra urna luctus. Proin sagittis, magna vehicula ullamcorper elementum, erat metus scelerisque lectus, id vestibulum velit sem condimentum neque. Mauris venenatis imperdiet mi.</p>
-			</div>
-			<div class="button-positie">
-				<a class="button" href="join.php">Speler</a>
-				<a class="button" href="create.php">Spelleider</a>
+			
+				<div class="button-positie">
+					<a class="button" href="join.php">Speler</a>
+					<a class="button" href="create.php">Spelleider</a>
 				
-				<?php
-				if (isset($_SESSION['game_id'])) {
-					$game = $_SESSION['game_id'];
-					$json = json_decode(file_get_contents("./games/$game.json"), true);
-					if ($json['game_started']) {
-						echo '<a class="button" href="./game.php">Naar spelpel</a> ';
-					} else {
-						echo '<a class="button" href="./lobby.php">Naar spel</a> ';
+					<?php
+					if (isset($_SESSION['game_id'])) {
+						$game = $_SESSION['game_id'];
+						$json = json_decode(file_get_contents("./games/$game.json"), true);
+						if ($json['game_started']) {
+							echo '<a class="button" href="./game.php">Naar spelpel</a> ';
+						} else {
+							echo '<a class="button" href="./lobby.php">Naar spel</a> ';
+						}
+						echo '<a class="button" href="./delete.php">Verwijder sessie</a>';
 					}
-					echo '<a class="button" href="./delete.php">Verwijder sessie</a>';
-				}
-				?>
+					?>
+				</div>
 			</div>
 		<?php include('../footer.php'); ?>
 	</body>
