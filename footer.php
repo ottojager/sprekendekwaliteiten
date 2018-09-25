1f5227ce96d870b7ab3b83b15fc950b4006a54ff
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2018-09-25 11:12:33 +0200

    This may lowkey be the final footer update (version σ/ς)

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer>
-	<p class="credits">"Sprekende kwaliteiten is uitgevoerd met toestemming van Peter Gerrickens en mogelijk gemaakt door Stichting Bartim&eacute;us Sonneheerdt en het KF Heinfonds" ©Eye4Web 2017-2018</p> <!-- <p class="copyright">©Eye4Web 2017-2018</p> -->
+	<p class="credits">Sprekende kwaliteiten is uitgevoerd met toestemming van Peter Gerrickens en mogelijk gemaakt door Stichting Bartim&eacute;us Sonneheerdt en het KF Heinfonds</p><p class="copyright">©Eye4Web 2017-2018</p>
 </footer>
