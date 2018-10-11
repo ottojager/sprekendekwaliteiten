faa63f96080df5e8f3320cc3f9e50df736f1bd57
nvmark <nielsberichten@gmail.com>
2018-10-11 12:01:08 +0200

    feedback end game view is now functionol I wish I could sleep for like a month

--- feedback/end/leader.php
+++ feedback/end/leader.php
@@ -2,31 +2,42 @@
 <html lang="nl=NL">
 <head>
 	<title>Einde - Feedback - Sprekende Kwaliteiten</title>
-	<link rel="stylesheet" href="../css/end.css" type="text/css">
+	<link rel="stylesheet" href="../../css/basis.css" type="text/css">
+	<link rel="stylesheet" href="../../css/header.css" type="text/css">
+	<link rel="stylesheet" href="../../css/footer.css" type="text/css">
 	<link rel="icon" sizes="16x16" type="image/png" href="/../kwal-spel/feedback/css/Rainbow_placeholder.png">
+	<script src="./endgame.js"></script>
+	<script>
+	var card_list =
+	<?php
+	foreach ($json['players'] as $key => $value) {
+		$array[$key] = $value['stack'];
+	}
+	echo json_encode($array);
+	?>
+	</script>
 </head>
-<body>
-	<div id="topbar"></div>
-	<div id="sidetopbar">
-		<div id="borderimage"></div>
-		<div id="player__name">Spelleider</div>
-	</div>
+<body onload="leader_initial_rendering_calls(<?php echo count($json['players']) -1; ?>)">
+	<?php
+	// create some variables to add header values
+	$spelvorm = 'Feedback';
+	$name = $_SESSION['player_name'];
+	$in_sub_folder = true;
+
+	include('../../header.php');
+	?>
 	<div id="container">
-		<h1>Einde - Feedback</h1>
+		<h2>Einde - Feedback</h2>
 		<?php
 		foreach ($json['players'] as $key => $player) {
 			if ($player['name'] != 'Afval stapel') {
-				echo '<h2>'.$player['name'].':</h2>';
-				echo '<ul>';
-				foreach ($player['stack'] as $key => $value) {
-					echo "<li>$value</li>";
-				}
-				echo '</ul>';
+				echo "<div id=\"card-list-container-$key\"></div>";
 			}
 		}
 		?>
 		<p>De spelers kunnen hun ontvangen kaarten per e-mail ontvangen en aan jou als spelleider doorsturen.</p>
 		<a href="../delete.php">Spel verlaten</a>
 	</div>
+	<?php include('../../footer.php'); ?>
 </body>
 </html>
--- feedback/end/players.php
+++ feedback/end/players.php
@@ -1,23 +1,47 @@
+<?php
+if (!isset($_SESSION['game_id'])) {
+	header('Location: ./');
+}
+$game = $_SESSION['game_id'];
+$json = json_decode(file_get_contents("../games/$game.json"), true);
+?>
 <!DOCTYPE html>
 <html lang="nl=NL">
 <head>
 	<title>Einde - Feedback - Sprekende Kwaliteiten</title>
-	<link rel="stylesheet" href="../css/end.css" type="text/css">
+	<!-- <link rel="stylesheet" href="../css/end.css" type="text/css"> -->
+	<link rel="stylesheet" href="../../css/basis.css" type="text/css">
+	<link rel="stylesheet" href="../../css/header.css" type="text/css">
+	<link rel="stylesheet" href="../../css/footer.css" type="text/css">
 	<link rel="icon" sizes="16x16" type="image/png" href="/../kwal-spel/feedback/css/Rainbow_placeholder.png">
-	<script src="./mail.js"></script>
+	<script src="./endgame.js"></script>
+	<script>
+	var card_list =
+	<?php
+	// just accept that this works
+	echo json_encode(
+		array(
+			$_SESSION['player_id'] => $json['players'][ $_SESSION['player_id'] ]['stack']
+		)
+	);
+	?>
+	</script>
 </head>
-<body>
-	<div id="topbar"></div>
-	<div id="sidetopbar">
-		<div id="borderimage"></div>
-		<div id="player__name"><?php echo $json['players'][ $_SESSION['player_id'] ]['name'];?></div>
-	</div>
-	<div id="container">
-		<h1>Einde spel - Feedback</h1>
-		<p id="uitlegtekst">Vul hier je email adres in, zodat je kaarten naar jou opgestuurd kunnen worden.</p>
-		<label for="email">Email</label>
-		<input name="mail" type="email" id="email" />
-		<button id="sendmail" onclick="send_mail()">Mail mijn kaarten</button>
-	</div>
+<body onload="render_card_list(0, <?php echo $_SESSION['player_id'] ?>)">
+	<?php
+	// create some variables to add header values
+	$spelvorm = 'Feedback';
+	$name = $_SESSION['player_name'];
+	$in_sub_folder = true;
+
+	include('../../header.php');
+	?>
+	<main id="main">
+		<h2>Ontvangen kaarten - Feedback</h2>
+		<div id="card-list-container-<?php echo $_SESSION['player_id'] ?>">
+
+		</div>
+	</main>
+	<?php include('../../footer.php'); ?>
 </body>
 </html>
