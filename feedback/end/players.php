fde5036ef99256d91255acb058c53f6b4507072f
nvmark <nielsberichten@gmail.com>
2018-10-18 11:22:05 +0200

    added cards existing on the end page

--- feedback/end/players.php
+++ feedback/end/players.php
@@ -12,20 +12,20 @@ $json = json_decode(file_get_contents("../games/$game.json"), true);
 	<link rel="stylesheet" href="../../css/footer.css" type="text/css">
 	<link rel="icon" sizes="16x16" type="image/png" href="/../kwal-spel/feedback/css/Rainbow_placeholder.png">
 	<script src="./endgame.js"></script>
-	<script>
+	<!-- <script>
 	var card_list =
 	<?php
 	// just accept that this works
-	echo json_encode(
-		array(
-			$_SESSION['player_id'] => $json['players'][ $_SESSION['player_id'] ]['stack']
-		)
-	);
+	// echo json_encode(
+	// 	array(
+	// 		$_SESSION['player_id'] => $json['players'][ $_SESSION['player_id'] ]['stack']
+	// 	)
+	// );
 	?>
-	</script>
+	</script> -->
 </head>
 <!-- javascript voor kaarten pagenation -->
-<!-- onload="render_card_list(0, <?php echo $_SESSION['player_id'] ?>, 8)" -->
+<!-- onload="render_card_list(0, <?php // echo $_SESSION['player_id'] ?>, 8)" -->
 <body>
 	<?php
 	// create some variables to add header values
@@ -37,8 +37,18 @@ $json = json_decode(file_get_contents("../games/$game.json"), true);
 	?>
 	<main class="container" id="main" tabindex="-1">
 		<h2>Ontvangen kaarten - Feedback</h2>
-		<div id="card-list-container-<?php echo $_SESSION['player_id'] ?>">
-
+		<div id="card-list-container">
+			<div class="kaart-rij">
+				<?php
+				foreach($json['players'][ $_SESSION['player_id'] ]['stack'] as $key => $value) {
+					if ($key % 4 == 0 && $key != 0) {
+						echo "</div>";
+						echo "<div class=\"kaart-rij\">";
+					}
+					echo "<li class=\"kaart eind-kaart\"><button>$value</button></li>";
+				}
+				?>
+			</div>
 		</div>
 	</main>
 	<?php include('../../footer.php'); ?>
