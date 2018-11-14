ebfb8ddc4048a541e3732e96feceeaea191bb385
headphoneGear <fkock@edu.bartimeus.nl>
2018-11-14 12:34:02 +0100

    test email stylen

--- bewustwording/mail.php
+++ bewustwording/mail.php
@@ -11,8 +11,153 @@ $message = '
 <html lang="nl=NL">
 <head>
 	<title>Uw kaarten</title>
+	
+	<style>
+	@import url("https://fonts.googleapis.com/css?family=Oswald:500|Roboto:500");
+	
+		body {
+			margin:0;
+		}
+
+		h1 {
+			color:#163143;
+			font-family: oswald, sans-serif;
+		}
+		
+		.header-content {
+			display: initial;
+			height: 112px;
+			margin: 0 0 0 auto;
+    		float: right;
+		}
+		
+		.header-content-title {
+			width:100%;
+			position:relative;
+			top: 15px;
+			left: 0%;
+		}
+		
+		.titel {
+			/*text*/
+			text-align:center;
+			font-family: "oswald",  monospace;
+			text-transform: uppercase;
+			letter-spacing: 1.0887244px;
+			font-weight: 100;
+			position: relative;
+			z-index: 1;
+			text-decoration: none;
+			
+			/*kleur*/
+			color:#264153;
+		}
+		
+		.boven {
+			font-size: 28px;
+			margin-bottom: -4px;
+			margin-top: -2px;
+		}
+		
+		.onder	{
+			font-weight: 100;
+			margin-top: 0;
+			letter-spacing: 0.3em;
+			font-size: 34px;
+			margin-bottom: 0px;
+		}
+		
+		header {
+			border-bottom:solid 3px #163143;
+			width:100%;
+			margin:0;
+			height:100px;	
+			background-image: url("https://northeurope1-mediap.svc.ms/transform/thumbnail?provider=spo&inputFormat=png&cs=fFNQTw&docid=https%3A%2F%2Fbartimeus-my.sharepoint.com%3A443%2F_api%2Fv2.0%2Fdrives%2Fb!rsOxxjbB6kacfzqYijN2qUD01OKGOpNNqDAaR0FVFns7BIJPLL02R5dAVQHXcrbq%2Fitems%2F01WIHRYN3QCU4OVXF7O5GZRH7U54W7YBRI%3Fversion%3DPublished&encodeFailures=1&ctag=%22c%3A%7BEA381570-BFDC-4D77-989F-F4EF2DFC0628%7D%2C4%22&width=1156&height=621&srcWidth=5274&srcHeight=2831");
+			background-size: 168px 88px;
+			background-repeat: no-repeat;
+			background-position: 12.2% 12%;
+			transition: background-position 1s;
+			position: relative;
+			background-color: white;
+		}
+		
+		footer {
+			background-color: #e9e9ea;
+			width: 100%;
+			font-size: 10px;
+			border-top: 4px solid #163143;
+			position: fixed;
+			bottom: 0;
+			left: 0;
+			height: 50px;
+			box-sizing: border-box;
+		}
+			
+		footer p {
+			color: #163143;
+			padding: 10px 0;
+			font-family: Oswald, monospace;
+			font-size: 1.1vw;
+			margin-bottom: 0px;
+		}
+			
+		.credits {
+			width: 100%;
+			text-align: center;
+		}
+			
+		.copyright {
+			position: absolute;
+			text-align: right;
+			height: 25px;
+			bottom: 0;
+			right: 4%;
+		}
+			
+		@media screen and (max-width: 948px) {
+		footer {
+			height: 70px;
+			margin-top: 20px;
+		}
+			
+		footer p {
+			padding: 3px 0;
+			font-size: 1.5vw;
+		}
+			
+		.copyright {
+			position: initial;
+			text-align: center;
+		}
+		}
+			
+		@media screen and (max-width: 686px) {
+			footer p {
+			font-size: 2vw;
+		}
+		}
+			
+		@media screen and (max-width: 468px) {
+			footer p {
+			font-size: 2.6vw;
+		}
+		}
+
+		
+	</style>
+	
 </head>
 <body>
+
+<header>
+<div class="header-content">
+		<div class="header-content-title">
+			<h1 class="titel boven">sprekende</h1>
+			<h1 class="titel onder">kwaliteiten</h1>
+		</div>
+	</div>
+</header>
+
 	<h1>Uw kaarten van Kwaliteitenspel - Bewustwording</h1>
 	<ul>';
 	foreach ($cards as $key => $value) {
@@ -21,8 +166,16 @@ $message = '
 		}
 	}
 $message .= '</ul>
+
+<footer>
+	<p class="credits">Sprekende kwaliteiten is uitgevoerd met toestemming van Peter Gerrickens en mogelijk gemaakt door Stichting Bartimeus Sonneheerdt en het KF Heinfonds</p><p class="copyright">©Eye4Web 2017-2018</p>
+</footer>
+
+
 </body>
-</html>';
+</html>
+
+';
 
 
 // Preferences
