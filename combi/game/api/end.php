ba4c3959fe48a61735337bb88218e40db97d8240
put <44380176+put@users.noreply.github.com>
2018-12-07 10:16:57 +0100

    Forgot to take my test echo out :(
    but its gone now

--- combi/game/api/end.php
+++ combi/game/api/end.php
@@ -5,7 +5,6 @@ if ($_SESSION['player_id'] == 11 && isset($_SESSION['game_id'])) {
 	$game = $_SESSION['game_id'];
 	$json = json_decode(file_get_contents("../../games/$game.json"), true);
 	$json['card_stack'] = array();
-	echo('CARD STACK SHOULD REALLY BE EMPTY.....');
 	file_put_contents("../../games/$game.json", json_encode($json));
 } else {
 	header('HTTP/1.1 403 Forbidden');
