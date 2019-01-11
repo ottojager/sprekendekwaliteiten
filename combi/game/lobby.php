438ac23c4d18d00412f56bdc138788b03d6161c2
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2019-01-11 10:01:54 +0100

    Lobby.php test!

--- combi/game/lobby.php
+++ combi/game/lobby.php
@@ -2,23 +2,23 @@
 session_start();
 
 if (!isset($_SESSION['game_id'])) {
-	header('location: ../');
+	header('location: ./');
 }
 
 // load game's json file
 $game = $_SESSION['game_id'];
-$json = json_decode(@file_get_contents("../games/$game.json"), true);
+$json = json_decode(@file_get_contents("./games/$game.json"), true);
 
 if (!(bool)$json) { // if $json actually has content
 	@unlink("../games/$game.json"); // delete the empty file if one were to exist
-	header('Location: ../delete.php'); // send the user to delete.php to have their session cleared
+	header('Location: ./delete.php'); // send the user to delete.php to have their session cleared
 }
 ?>
 <!DOCTYPE html>
-<html lang="nl=NL">
+<html lang="nl">
 	<head>
 		<title>Spelvoorbereiding - Feedback - Sprekende Kwaliteiten</title>
-		<script src="./api/js/std.js"></script>
+		<script src="api/js/std.js"></script>
 
 		<?php
 		// java script only needed for the game leader
@@ -29,7 +29,7 @@ if (!(bool)$json) { // if $json actually has content
 					var xhttp = new XMLHttpRequest();
 					xhttp.onreadystatechange = function() {
 						if (this.readyState == 4 && this.status == 200) {
-							window.location.href = './';
+							window.location.href = 'game.php';
 						}
 					};
 					xhttp.open("GET", "./api/start.php", true);
@@ -48,10 +48,18 @@ if (!(bool)$json) { // if $json actually has content
 			var id = <?php echo $_SESSION['player_id']; ?>; // the ID of the current user
 
 			var s = 3000; // how often to refresh in ms
+			var last_change = 0;
 			window.setInterval(function(){ // run this code every [s] seconds
 				start_update(); // get the new game_info
+
+				if (last_change == game_info['last_change']) {
+					return
+				} else {
+					last_change = game_info['last_change'];
+				}
+
 				if (game_info["game_started"] == true) {
-					window.location.href = './'; // redirect users if the game has been started
+					window.location.href = 'game.php'; // redirect users if the game has been started
 				}
 
 				// regenerate the player list
@@ -67,7 +75,7 @@ if (!(bool)$json) { // if $json actually has content
 						button.onclick = function() {
 							kick(index);
 						};
-						button.innerHTML = '<img src="../../afbeeldingen/remove-knop.png" alt="Verwijder speler" height="20" width="20">';
+						button.innerHTML = '<img src="../afbeeldingen/remove-knop.png" alt="Verwijder speler" height="20" width="20">';
 						child.appendChild(button);
 					<?php } ?>
 
@@ -76,10 +84,11 @@ if (!(bool)$json) { // if $json actually has content
 			}, s);
 		</script>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="../../css/header.css" type="text/css">
-		<link rel="stylesheet" href="../../css/basis.css" type="text/css">
-		<link rel="stylesheet" href="../../css/footer.css" type="text/css">
-		<link rel="stylesheet" href="../../css/spelvorm2.css" type="text/css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../css/header.css" type="text/css">
+		<link rel="stylesheet" href="../css/basis.css" type="text/css">
+		<link rel="stylesheet" href="../css/footer.css" type="text/css">
+		<link rel="stylesheet" href="../css/spelvorm2.css" type="text/css">
 		<link rel="icon" sizes="16x16" type="image/png" href="css/Rainbow_placeholder.png">
 	</head>
 	<body>
@@ -89,7 +98,7 @@ if (!(bool)$json) { // if $json actually has content
 		$spelvorm = 'Feedback';
 		$name = $_SESSION['player_name'];
 
-		include('../../header.php');
+		include('../header.php');
 		?>
 		<main class="container" id="main" tabindex="-1">
 			<?php if ($_SESSION['player_id'] == 11) { // game leader only ?>
@@ -99,6 +108,7 @@ if (!(bool)$json) { // if $json actually has content
 			De volgende spelers doen mee:</p>
 
 			<?php } else { // player only ?>
+			<h2>Spelvoorbereiding - Feedback</h2>
 			<p>De volgende spelers doen mee:</p>
 			<?php } ?>
 
@@ -106,7 +116,7 @@ if (!(bool)$json) { // if $json actually has content
 			<?php
 			foreach ($json['players'] as $key => $value) {
 				if ($_SESSION['player_id'] == 11) {
-					echo '<li>'.$value['name'].'<button onclick="kick(\''.$key.'\')"><img src="../../afbeeldingen/remove-knop.png" alt="Verwijder speler" height="20" width="20"></button></li>';
+					echo '<li>'.$value['name'].'<button onclick="kick(\''.$key.'\')"><img src="../afbeeldingen/Remove-knop.png" alt="Verwijder speler" height="20" width="20"></button></li>';
 				} else {
 					echo '<li>'.$value['name'].'</li>';
 				}
@@ -117,10 +127,10 @@ if (!(bool)$json) { // if $json actually has content
 		if ($_SESSION['player_id'] == 11) {
 			?><div class="button"> <button id="start" onclick="start_game()">Start het spel</button></div><?php
 		} else {
-			?><p>Wacht tot iedereen aanwezig is. De spelbegeleider zal het spel zometeen beginnen.</p><?php
+			?><p class="wacht-text">Wacht tot iedereen aanwezig is. De spelbegeleider zal het spel zometeen beginnen.</p><?php
 		}
 		?>
 		</main>
-		<?php include('../../footer.php') ?>
+		<?php include('../footer.php') ?>
 	</body>
 </html>
