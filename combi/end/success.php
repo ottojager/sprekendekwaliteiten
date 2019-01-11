599fec76c148a438c64a7f509479e1fb67a526c0
Sander <35995707+SanderinnoRipperinno@users.noreply.github.com>
2019-01-11 09:32:55 +0100

    success.php bestand miste

---
+++ combi/end/success.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<link rel="stylesheet" href="../../css/basis.css" type="text/css">
+	<link rel="stylesheet" href="../../css/header.css" type="text/css">
+	<link rel="stylesheet" href="../../css/footer.css" type="text/css">
+	<link rel="stylesheet" href="../../css/spelvorm2.css" type="text/css">
+</head>
+<body>
+	<?php
+	$spelvorm = 'Feedback';
+	$name = $_SESSION['player_name'];
+	$in_sub_folder = true;
+
+	include('../../header.php');
+	?>
+	<main id="main">
+		<p class="success">De email is verstuurd</p>
+		<div class="button">
+			<button class="back-button" onclick="window.location='../index.php'">Terug naar home</button>
+		</div>
+	</main>
+	<?php include('../../footer.php'); ?>
+</body>
+</html>
