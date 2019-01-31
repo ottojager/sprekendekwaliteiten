63f4223c2bddbbd0dc759caa6b535d5cf67e47f7
put <44380176+put@users.noreply.github.com>
2019-01-31 15:25:29 +0100

    Fixed potential browser crashes in feedback
    Basically the amount of event handlers on buttons would be multiplied by itself every time. You'll end up with millions of handlers in a few clicks, which (obviously) crashes your browser. This should fix :+1:

--- feedback/api/js/std.js
+++ feedback/api/js/std.js
@@ -1,3 +1,5 @@
+var lastClickedLeaderId = -1;
+
 function reply_click(clicked_id) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", "./api/game_logic.php?sel=" + clicked_id, true);
@@ -41,40 +43,40 @@ function leader_card(amount_players) {
 	for (i = 0; i <  amount_players; i += 1) {
 		document.getElementById(i.toString()).addEventListener('click', function() {
 			leader_view_cards(this.id);
+			console.log(this.id);
 		});
 	}
 }
 
 function leader_view_cards(id_player) {
-	selected_player = id_player;
-	var div = document.getElementById('card_stack');
-	div.innerHTML = '';
+	if (lastClickedLeaderId != id_player) {
+		playerId = id_player.toString();
+		selected_player = id_player;
+		var div = document.getElementById('card_stack');
+		div.innerHTML = '';
 
-	var name = document.createElement('h3');
-	if (game_info['players'][id_player]['name'] != 'Afval stapel') {
-		name.innerHTML = game_info['players'][id_player]['name'];
-	} else {
-		name.innerHTML = 'Aflegstapel';
+		var name = document.createElement('h3');
+		if (game_info['players'][id_player]['name'] != 'Afval stapel') {
+			name.innerHTML = game_info['players'][id_player]['name'];
+		} else {
+			name.innerHTML = 'Aflegstapel';
+		}
+		var list = document.createElement('ul');
+		game_info['players'][id_player]['stack'].forEach(function(item, index){
+			var child = document.createElement('li');
+			child.innerHTML = item;
+			list.appendChild(child);
+		});
+
+		div.appendChild(name);
+		div.appendChild(list);
+
+		lastClickedLeaderId = id_player;
 	}
-	var list = document.createElement('ul');
-	game_info['players'][id_player]['stack'].forEach(function(item, index){
-		var child = document.createElement('li');
-		child.innerHTML = item;
-		list.appendChild(child);
-	});
-
-	div.appendChild(name);
-	div.appendChild(list);
-	document.getElementById(id_player).addEventListener('click', function() {
-		leader_view_cards_hide(this.id);
-	});
-}
-
-function leader_view_cards_hide(id_player) {
-	document.getElementById('card_stack').innerHTML = '';
-	document.getElementById(id_player).addEventListener('click', function() {
-		leader_view_cards(this.id);
-	})
+	else {
+		document.getElementById('card_stack').innerHTML = '';
+		lastClickedLeaderId = -1;
+	}
 }
 
 function update_view() {
