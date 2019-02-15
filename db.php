1fab5a24140d84eacc25edd1bc2e455d1e59c45d
put <44380176+put@users.noreply.github.com>
2019-02-15 10:58:43 +0100

    Test pdo conn

--- db.php
+++ db.php
@@ -11,6 +11,8 @@ $options = [
 ];
 try {
      $pdo = new PDO($dsn, $config['username'], $config['password'], $options);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
--- login_check.php
+++ login_check.php
@@ -2,6 +2,21 @@
 if (!isset($_SESSION['logged_in'])) {
     include('db.php');
     $query = $pdo->prepare('SELECT value FROM config WHERE name=?');
+/*
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'localonly', 'localonly');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    
+    
+        $pdo->prepare('INSERT INTO DoesNotExist (x) VALUES (?)');
+    }
+    catch(Exception $e) {
+        echo 'Exception -> ';
+        var_dump($e->getMessage());
+    }
+*/
+
     $query->execute(["LOGIN_URL"]);
     $_SESSION['login_url'] = $query->fetch()['value'];
     $_SESSION['logged_in'] = 'not ok';
