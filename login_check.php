013b47ae6c5aada43806010d59bc56592b9bbc2d
put <44380176+put@users.noreply.github.com>
2019-02-15 11:03:57 +0100

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -2,23 +2,14 @@
 if (!isset($_SESSION['logged_in'])) {
     include('db.php');
     $query = $pdo->prepare('SELECT value FROM config WHERE name=?');
-
-    try {
-        $query = $pdo->prepare('SELECT value FROM config WHERE name=?');
-        $query->execute(["LOGIN_URL"]);
-    }
-    catch(Exception $e) {
-        echo 'Exception -> ';
-        var_dump($e->getMessage());
-    }
-
+    $query->execute(["LOGIN_URL"]);
     $_SESSION['login_url'] = $query->fetch()['value'];
     $_SESSION['logged_in'] = 'not ok';
-    echo('we set it to not ok');
+    echo 'we set it to not ok';
 }
 
 if ($_SESSION['logged_in'] == 'not ok' && !isset($isLoginPage)) {
-    //echo("check ".$_SESSION['logged_in']);
+    echo "check ".$_SESSION['logged_in'];
     header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SESSION['login_url']);
 }
 ?>
