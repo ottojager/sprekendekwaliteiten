0452f483ac3adbe8b93a5c319598c39b87a8b32f
put <44380176+put@users.noreply.github.com>
2019-02-21 13:42:12 +0100

    updated database structures and file names
    not sure if they're even used but hey here you go

---
+++ sql/database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cards (
+	ID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name varchar(30) NOT NULL,
+	type INT(1) NOT NULL DEFAULT '1'
+);
+
+CREATE TABLE users (
+	id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	username VARCHAR(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	password VARCHAR(40) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+);
