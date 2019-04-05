5779a88b3ef84914359df5658c0c925f9357d7f4
put <44380176+put@users.noreply.github.com>
2019-04-05 15:35:31 +0200

    Moved mail stuff into another folder and made MailBuilder actually build mails!
    :)

---
+++ mail/MailBuilder.php
@@ -0,0 +1,89 @@
+<?php
+include('HtmlElement.php');
+
+class MailBuilder {
+    public $root;
+    private $head;
+    private $body;
+    private $mailHeaders;
+    private $subjectPrefs;
+    private $subject;
+
+    public function __construct()
+    {
+        $this->root = HtmlElement::Create("html")->setLang("nl=NL");
+        $this->head = $this->root->addHead();
+        $this->head->addStyle()->setType("text/css")->text(file_get_contents("style.css"));
+        $this->body = $this->root->addBody();
+
+        $this->mailHeaders = ["Content-Transfer-Encoding" => "8bit",
+        "Content-type" => "text/html; charset=urf-8",
+        "MIME-Version" => "1.0"];
+
+        $this->subjectPrefs = ["input-charset" => "utf-8",
+        "output-charset" => "urf-8",
+        "line-length" => 76,
+        "line-break-chars" => "\r\n"];
+
+        $titles = $this->body->addDiv()->setClass("header")->addDiv()->setClass("header-content")->addDiv()->setClass("header-content-title");
+        $titles->addH1()->setClass("titel boven")->text("sprekende");
+        $titles->addH1()->setClass("titel onder")->text("kwaliteiten");
+    }
+
+    public function setTitle($gameMode)
+    {
+        $this->body = $this->body->addCenter();
+        $message = "Jouw 'Sprekende Kwaliteiten' - $gameMode";
+        $this->body->addH1()->text($message);
+        $this->subject = $message;
+    }
+
+    public function setCustomMailHeader($name, $value)
+    {
+        $this->mailHeaders[$name] = $value;
+    }
+
+    public function setCustomSubjectPreferences($name, $value)
+    {
+        $this->subjectPrefs[$name] = $value;
+    }
+
+    public function insertCards($cardArray)
+    {
+        $cardList = $this->body->addUl();
+        foreach ($cardArray as $key => $value)
+        {
+            if ($value != '')
+            {
+                $cardList->addLi()->setClass("kaart")->addDiv()->setClass("kaartContainer")->addParagraph()->text($value);
+            }
+        }
+    }
+
+    private function getHeaderString()
+    {
+        $result = [];
+
+        $this->mailHeaders["Date"] = date("r (T)");
+        $this->mailHeaders["Subject"] = str_replace("Subject: ", "", iconv_mime_encode("Subject", $this->subject, $this->subjectPrefs));
+
+        foreach ($this->mailHeaders as $key => $value)
+        {
+            $result[] = "$key: $value";
+        }
+
+        return implode("\r\n", $result);
+    }
+
+    public function sendMail($recipient)
+    {
+        echo $this->root;
+        //mail($recipient, $this->subject, (string)$this->root, getHeaderString());
+        //for debugging purposes we're not actually mailing anything right now!
+    }
+}
+
+$mail = new MailBuilder();
+$mail->setTitle("Feedback");
+$mail->insertCards(["test", "test2", "test3"]);
+$mail->sendMail("fake-email");
